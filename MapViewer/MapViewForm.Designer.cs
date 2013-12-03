9a0d2068dff9f5fb0fa15a0470397e3ac5496c5b
Shinya.Kumagai <roomful.rooms@gmail.com>
2013-12-03 23:57:17 +0900

    Added view classes
    コントロールはまだグラフィックスライブラリの調査中のため実装していない。

---
+++ MapViewer/MapViewForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MapViewer
+{
+    partial class MapViewForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.OnClickOpenFile);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // MapViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MapViewForm";
+            this.Text = "KmlMapViewer";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ MapViewer/MapViewForm.cs
@@ -0,0 +1,124 @@
+﻿using MapViewer.Converter;
+using MapViewer.Loader;
+using MapViewer.Renderer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapViewer
+{
+    /// <summary>
+    /// 地図を表示するためのフォーム
+    /// </summary>
+    public partial class MapViewForm : Form
+    {
+        #region Private fields
+
+        /// <summary>
+        /// 描画するポリゴン
+        /// </summary>
+        private IList<Geometory.Polygon> _drawPolygons;
+
+        /// <summary>
+        /// ポリゴンのレンダラ
+        /// </summary>
+        private IRenderer _renderer;
+
+        #endregion
+
+        #region Constructor
+
+        public MapViewForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// メニューの「ファイルを開く」をクリックしたときに呼ばれる処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnClickOpenFile(object sender, EventArgs e)
+        {
+            // KMLファイルを読み込むための設定
+            openFileDialog.Title = "KMLファイルを選択してください";
+            openFileDialog.FileName = "map.kml";
+            openFileDialog.Filter = "KMLファイル(*.kml)|*.kml";
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            IList<Geometory.Polygon> polygons = null;
+
+            Cursor = Cursors.WaitCursor;
+            
+            Task.Factory.StartNew(() =>
+                {
+                    // ファイルからポリゴンを作成する
+                    return new KmlLoader().Load(openFileDialog.FileName);
+                })
+            .ContinueWith((t) =>
+                {
+                    // 読み込んだ地図を座標変換する
+                    var converter = new PolygonConverter()
+                    {
+                        Polygons = t.Result,
+                        DisplaySize = Math.Min(ClientSize.Width, ClientSize.Height),
+                    };
+                    return polygons = converter.Convert();
+                })
+            .ContinueWith((t) =>
+                {
+                    _drawPolygons = t.Result;
+                    Console.WriteLine("Finished {0} polygons", polygons.Count);
+                    // 異なる地図を描画する場合は以前の描画をクリアしておく
+                    CreateGraphics().Clear(BackColor);
+                    Invalidate();
+                    // 終了時にカーソルを戻す
+                    Cursor = Cursors.Default;
+                }, 
+                TaskScheduler.FromCurrentSynchronizationContext()
+            );
+        }
+
+        /// <summary>
+        /// フォームがロードされたときに呼ばれる処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnLoad(object sender, EventArgs e)
+        {
+            _drawPolygons = new List<Geometory.Polygon>();
+            _renderer = new GraphicsRenderer();
+
+            // クライアント領域のサイズを設定する
+            SizeFromClientSize(new Size(1000, 1000));
+
+            //画面がちらつかないようにする
+            SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+       
+        /// <summary>
+		/// 画面を描画するときに呼ばれる
+		/// </summary>
+		/// <param name="e"></param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (_drawPolygons.Count > 0)
+            {
+                _renderer.Render(e.Graphics,_drawPolygons);
+            }
+            base.OnPaint(e);
+        }
+    }
+}
