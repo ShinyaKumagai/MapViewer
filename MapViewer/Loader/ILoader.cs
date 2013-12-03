cd40bb6ce6cdede4afc42e99a7f7a41793da8f80
Shinya.Kumagai <roomful.rooms@gmail.com>
2013-12-03 23:54:41 +0900

    Added loader classes of the file
    KMLファイルのローダのみ実装している。
    その他のローダは現状作成する予定なし。

---
+++ MapViewer/Loader/ILoader.cs
@@ -0,0 +1,27 @@
+﻿using MapViewer.Geometory;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapViewer.Loader
+{
+    /// <summary>
+    /// 地図データを読み込むためのインターフェース
+    /// </summary>
+    public interface ILoader
+    {
+        #region Public methods
+
+        /// <summary>
+        /// 地図データから座標データを読み込む
+        /// </summary>
+        /// <param name="path">ファイルパス</param>
+        /// <returns>座標データ</returns>
+        IList<Polygon> Load(string path);
+
+        #endregion
+    }
+}
---
+++ MapViewer/Loader/KmlLoader.cs
@@ -0,0 +1,73 @@
+﻿using SharpKml.Base;
+using SharpKml.Dom;
+using SharpKml.Engine;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapViewer.Loader
+{
+    /// <summary>
+    /// KMLファイルから地図データを読み込むためのローダ
+    /// </summary>
+    public class KmlLoader : ILoader
+    {
+        #region ILoader methods
+
+        public IList<Geometory.Polygon> Load(string path)
+        {
+            Kml kml = null;
+            try
+            {
+                kml = ParseLegacyFile(path).Root as Kml;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            return ToPolygons(kml);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private IList<Geometory.Polygon> ToPolygons(Kml kml)
+        {
+            var polygons = new List<Geometory.Polygon>();
+
+            // KMLファイルからポリゴンを構成する座標を取得する
+            foreach (var coords in kml.Feature.Flatten().OfType<CoordinateCollection>())
+            {
+                var polygon = new Geometory.Polygon();
+                var points = from c in coords
+                             select new PointF((float)c.Longitude, (float)c.Latitude);
+                polygon.Points = points.ToList();
+
+                polygons.Add(polygon);
+            }
+
+            return polygons;
+        }
+
+        private KmlFile ParseLegacyFile(string path)
+        {
+            // Manually parse the Xml
+            string xml = File.ReadAllText(path);
+
+            var parser = new Parser();
+            // Ignore the namespaces
+            parser.ParseString(xml, false);
+
+            // The element will be stored in parser.Root - wrap it inside a KmlFile.
+            return KmlFile.Create(parser.Root, true);
+        }
+
+        #endregion
+    }
+}
