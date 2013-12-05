4a5b3482eb578663f624753f49186256ef8075f6
Shinya.Kumagai <roomful.rooms@gmail.com>
2013-12-05 22:21:14 +0900

    Updated the converter class
    PointF→Vector2のコンバータを作成した。

---
+++ MapViewer/Provider/Converter/ScreenConverter.cs
@@ -0,0 +1,103 @@
+﻿using MapViewer.Geometory;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapViewer.Converter
+{
+    /// <summary>
+    /// ポリゴンを画面座標に変換する
+    /// </summary>
+    public class ScreenConverter : IConverter<Polygon>
+    {
+        #region Public properties
+
+        /// <summary>
+        /// 表示領域（高さ、幅）の短いほうの長さ
+        /// </summary>
+        /// <remarks>
+        /// ポリゴンの縮尺を計算するために使用する
+        /// </remarks>
+        public float DisplayLength
+        {
+            private get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// 新しいインスタンスを作成する
+        /// </summary>
+        public ScreenConverter() :
+            this(1f)
+        {
+        }
+
+        /// <summary>
+        /// 新しいインスタンスを作成する
+        /// </summary>
+        /// <param name="srcPolygons">変換元のポリゴン</param>
+        /// <param name="displayLength"></param>
+        public ScreenConverter(float displayLength)
+        {
+            DisplayLength = displayLength;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        #region IConverter
+
+        public IList<Polygon> Convert(IList<Polygon> sourcePolygons)
+        {
+            if (sourcePolygons == null || sourcePolygons.Count == 0)
+            {
+                return　null;
+            }
+
+            // 全ポリゴンの表示範囲を計算する
+            var min = new PointF(float.MaxValue, float.MaxValue);
+            var max = new PointF(float.MinValue, float.MinValue);
+
+            foreach (var polygon in sourcePolygons)
+            {
+                foreach (var p in polygon)
+                {
+                    min.X = Math.Min(min.X, p.X);
+                    min.Y = Math.Min(min.Y, p.Y);
+                    max.X = Math.Max(max.X, p.X);
+                    max.Y = Math.Max(max.Y, p.Y);
+                }
+            }
+
+            // ポリゴンを画面内に収めるための縮尺を計算する。
+            float ratio = DisplayLength / Math.Max(max.X - min.X, max.Y - min.Y);
+
+            var destPolygons = new List<Polygon>(sourcePolygons.Count);
+            // 左下を原点とした座標に変換する。
+            foreach (var srcPolygon in sourcePolygons)
+            {
+                var destPolygon = new Polygon();
+                var points = srcPolygon.Select(p => new PointF()
+                                                     {
+                                                         X = -(min.X - p.X) * ratio,
+                                                         Y =  (max.Y - p.Y) * ratio
+                                                     });
+                destPolygon.Points = points.ToList();
+                destPolygons.Add(destPolygon);
+            }
+            return destPolygons;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ MapViewer/Provider/Converter/VectorConverter.cs
@@ -0,0 +1,76 @@
+﻿using MapViewer.Geometory;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapViewer.Converter
+{
+    /// <summary>
+    /// <see cref="Vector2"/>型のポリゴン変換する
+    /// </summary>
+    /// <remarks>
+    /// <see cref="IProvider"/>で作成するポリゴンは<see cref="PointF"/>型なので
+    /// OpenGL用の座標にに変化する必要がある。
+    /// </remarks>
+    public class VectorConverter : IConverter<GLPolygon>
+    {
+        #region Public properties
+
+        /// <summary>
+        /// 変換対象のポリゴン
+        /// </summary>
+        public IList<Polygon> SourcePolygons
+        {
+            private get;
+            set;
+        }
+
+        #endregion
+
+        #region Constrcutor
+
+        /// <summary>
+        /// 新しいインスタンスを作成する
+        /// </summary>
+        public VectorConverter() :
+            this(new List<Polygon>())
+        {
+
+        }
+
+        /// <summary>
+        /// 新しいインスタンスを作成する
+        /// </summary>
+        /// <param name="srcPolygon">変換元のポリゴン</param>
+        public VectorConverter(IList<Polygon> srcPolygon)
+        {
+
+        }
+
+        #endregion
+
+        #region IConverter
+
+        public IList<GLPolygon> Convert(IList<Polygon> sorucePolygons)
+        {
+            if (SourcePolygons == null || SourcePolygons.Count == 0)
+            {
+                return null;
+            }
+
+            var destPolygons = new List<GLPolygon>(SourcePolygons.Count);
+            // PointF型の座標をVector2型に変換する。
+            foreach (var srcPolygon in SourcePolygons)
+            {
+                var destPolygon = new GLPolygon();
+                destPolygon.Points =  srcPolygon.Select(p => new Vector2(p.X, p.Y)).ToList();
+                destPolygons.Add(destPolygon);
+            }
+            return destPolygons;
+        }
+
+        #endregion
+    }
+}
