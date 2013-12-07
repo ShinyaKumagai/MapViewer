73f8216209a73011b9bb1f8afe6288d4e5381195
Shinya Kumagai <roomful.rooms@gmail.com>
2013-12-08 05:08:22 +0900

    Added the vertex class
    現在はVertexPosition, VertexPositionColorの未実装。
    いずれも2Dようになっている。

---
+++ OpenGL/Vertex/IVertexAttribute.cs
@@ -0,0 +1,30 @@
+﻿using MapViewer.OpenGL.VBO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapViewer.OpenGL.Vertex
+{
+    /// <summary>
+    /// 頂点属性をバインドする時の設定
+    /// </summary>
+    /// <remarks>
+    /// 頂点がどのような情報を使用するか定める
+    /// </remarks>
+    public interface IVertexAttribute : IBindable
+    {
+        #region Public properties
+
+        /// <summary>
+        /// 頂点のサイズを取得する
+        /// </summary>
+        int SizeInBytes
+        {
+            get;
+        }
+
+        #endregion
+    }
+}
---
+++ OpenGL/Vertex/VertexPositionColorTexture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapViewer.OpenGL.Vertex
+{
+    /// <summary>
+    /// 位置、描画色、テクスチャ座標の頂点情報
+    /// </summary>
+    public struct VertexPositionColorTexture
+    {
+    }
+}
